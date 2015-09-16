bba41ea251ef01ca074bb5c3fa23bd0c2025259e
Cristan <cristanz@yahoo-inc.com>
2015-09-16 12:56:31 -0700

    Step1: setup project with required pod files

---
+++ ParseChat/ParseChat/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ParseChat
+//
+//  Created by Cristan Zhang on 9/16/15.
+//  Copyright (c) 2015 FastTrack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
